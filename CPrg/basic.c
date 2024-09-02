dbe63fe37c3428c23811282724cb8e4579e9e9cb
Anjana-lad <iantanjanalad@gmail.com>
2024-09-02 16:32:53 +0530

    c code

---
+++ CPrg/basic.c
@@ -0,0 +1,17 @@
+// structure of c program.
+/*
+1. documentation section.
+2. link section.  //
+3. global declaration section.
+4. main(){   //
+    // body of the program.
+return 0;
+}
+user defined function.
+*/
+
+// program to print hello world
+#include<stdio.h>
+void main(){
+    printf("Hello world..");
+}
---
+++ CPrg/datat ype.c
@@ -0,0 +1,32 @@
+// program to show datatype
+#include<stdio.h>
+int main(){
+   int num;   
+   char ch;
+   //float salary;
+
+   printf("Enter character:");
+   scanf("%c",&ch);
+   printf("Character=%c",ch);
+    printf("Enter number:");
+    scanf("%d",&num);
+    printf("Number=%d",num);
+return 0;
+}
+
+// formatted i/o function.
+// 1. printf();  // output function
+// 2. scanf();  // input function
+
+
+/* format specifier
+  int = %d 
+  char =%c
+  float =%f
+  long =%lf;
+  double =%ld;
+  string =%s;
+  pointer =%p;
+
+
+*/
